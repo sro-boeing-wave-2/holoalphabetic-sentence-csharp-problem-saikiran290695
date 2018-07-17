ab85da8ea64d9036d60e9e41da57adffde168890
vasamsettisaikiran <vasamsettisaikiran@yahoo.com>
2018-07-17 19:51:36 +0530

    used regex to remove unwanted char

--- HoloalphabeticSentence/HoloalphabeticSentence.cs
+++ HoloalphabeticSentence/HoloalphabeticSentence.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace HoloalphabeticSentence
 {
@@ -8,7 +9,14 @@ namespace HoloalphabeticSentence
         // Write the logic to determine whether the input string is a HoloalphabeticSentence or not
         public static bool IsHoloalphabeticSentence(string input, string alphabets = "abcdefghijklmnopqrstuvwxyz")
         {
-            return false;
+            bool count = false;
+            Regex reg = new Regex("[^A-Z]+");
+            var unique = reg.Replace(input.ToUpper(), "").ToCharArray().Distinct();
+            if (unique.Count() == 26)
+            {
+                count = true;
+            }
+            return count;
         }
     }
 }
