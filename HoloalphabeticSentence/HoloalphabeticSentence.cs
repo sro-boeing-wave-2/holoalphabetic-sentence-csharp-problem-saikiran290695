0183314226ab52b98998d225b5e1ad2b51b6b457
vasamsettisaikiran <vasamsettisaikiran@yahoo.com>
2018-07-23 00:27:26 +0530

    IF loop replaced with ? operator

--- HoloalphabeticSentence/HoloalphabeticSentence.cs
+++ HoloalphabeticSentence/HoloalphabeticSentence.cs
@@ -9,13 +9,9 @@ namespace HoloalphabeticSentence
         // Write the logic to determine whether the input string is a HoloalphabeticSentence or not
         public static bool IsHoloalphabeticSentence(string input, string alphabets = "abcdefghijklmnopqrstuvwxyz")
         {
-            bool count = false;
             Regex reg = new Regex("[^A-Z]+");
             var unique = reg.Replace(input.ToUpper(), "").ToCharArray().Distinct();
-            if (unique.Count() == 26)
-            {
-                count =  true;
-            }
+            bool count = (unique.Count() == 26) ? true : false;
             return count;
         }
     }
